f968ef2334ae70a3ce0917faf4f3552dd54f7cd8
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2024-03-06 11:07:16 +0700

    update terraform karpenter

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ locals {
   ## ADDON Version
   #============================================
   karpenter = {
-    version = "v0.35.0"
+    version = "v0.33.2"
   }
   argocd = {
     version = "6.5.1"
