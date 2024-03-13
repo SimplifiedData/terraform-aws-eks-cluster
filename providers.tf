dd700e38966d5f954f8a58504c1547e27bf18ad0
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2024-03-13 10:03:13 +0700

    update

--- providers.tf
+++ providers.tf
@@ -20,7 +20,7 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-
+    token                  = data.aws_eks_cluster_auth.this.token
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
