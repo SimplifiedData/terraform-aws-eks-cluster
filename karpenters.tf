7375b3da064214f2811e7bd11b08b49a8589178b
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2024-05-08 20:44:51 +0700

    update subnets

--- karpenters.tf
+++ karpenters.tf
@@ -77,7 +77,9 @@ spec:
   amiFamily: AL2
   associatePublicIPAddress: false
   subnetSelectorTerms:
-    - id: "%{~for i, v in data.aws_subnets.nonexpose.ids~}${v}%{if i < length(data.aws_subnets.nonexpose.ids) - 1}, %{endif}%{~endfor~}"
+    - tags:
+        Name: "*isearch-gs-sds-sss-dev-nonexpose*"
+    # - id: "%{~for i, v in data.aws_subnets.nonexpose.ids~}${v}%{if i < length(data.aws_subnets.nonexpose.ids) - 1}, %{endif}%{~endfor~}"
   securityGroupSelectorTerms:
     - tags:
         karpenter.sh/discovery: ${module.eks.cluster_name}
