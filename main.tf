5394cd0f6e9e8bb0c18b67109029bce533aa5b6d
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2024-06-17 01:12:58 +0700

    update

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "eks" {
 
   vpc_id     = var.vpc_id
   subnet_ids = data.aws_subnets.nonexpose.ids
-  # control_plane_subnet_ids = data.aws_subnets.nonexpose.ids
+  control_plane_subnet_ids = data.aws_subnets.nonexpose.ids
   # Fargate profiles use the cluster primary security group so these are not utilized
   create_cluster_security_group = var.enable_node_group == true ? true : false
   create_node_security_group    = var.enable_node_group == true ? true : false
