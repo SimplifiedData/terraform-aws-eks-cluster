a9d25d698a97fb3c86a019dc29531d482b417fd2
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2024-06-17 13:18:55 +0700

    update

--- karpenters.tf
+++ karpenters.tf
@@ -5,35 +5,6 @@ locals {
 }
 
 # Kube Apply CRDs
-data "http" "provisioners" {
-  url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.32.7/pkg/apis/crds/karpenter.sh_provisioners.yaml"
-  request_headers = {
-    Accept = "text/plain"
-  }
-}
-resource "kubectl_manifest" "provisioners" {
-  yaml_body = data.http.provisioners.body
-}
-
-data "http" "machines" {
-  url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.32.7/pkg/apis/crds/karpenter.sh_machines.yaml"
-  request_headers = {
-    Accept = "text/plain"
-  }
-}
-resource "kubectl_manifest" "machines" {
-  yaml_body = data.http.machines.body
-}
-
-data "http" "awsnodetemplates" {
-  url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.32.7/pkg/apis/crds/karpenter.k8s.aws_awsnodetemplates.yaml"
-  request_headers = {
-    Accept = "text/plain"
-  }
-}
-resource "kubectl_manifest" "awsnodetemplates" {
-  yaml_body = data.http.awsnodetemplates.body
-}
 data "http" "nodepools" {
   # url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.32.7/pkg/apis/crds/karpenter.sh_nodepools.yaml"
   url = "https://raw.githubusercontent.com/aws/karpenter/v${local.karpenter["version"]}/pkg/apis/crds/karpenter.sh_nodepools.yaml"
