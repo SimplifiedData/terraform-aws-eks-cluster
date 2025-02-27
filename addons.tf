610e1fb6c6892110705fa9df5f0e789cba979bd0
chanvitrun <chanvitrun@gosoft.co.th>
2025-02-27 23:30:33 +0700

    EKS BluePrint to 1.17.0

--- addons.tf
+++ addons.tf
@@ -117,7 +117,7 @@ module "eks_blueprints_addons" {
 
 module "eks_blueprints_addons_system" {
   source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "1.16.0"
+  version = "1.17.0"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
